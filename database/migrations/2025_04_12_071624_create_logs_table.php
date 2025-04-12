60e1f6847389fe6bcf8f53476aa73b3712991181
tansibMuttakin <tansib.muhaimin@gmail.com>
2025-04-12 18:37:24 +0600

    add columns on logs table

--- database/migrations/2025_04_12_071624_create_logs_table.php
+++ database/migrations/2025_04_12_071624_create_logs_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,10 @@ return new class extends Migration
     {
         Schema::create('logs', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('file_id')->constrained()->cascadeOnDelete();
+            $table->string('action')->nullable(); #file claimed, edited or completed
+            $table->string('details')->nullable();
             $table->timestamps();
         });
     }

2e3c17a77b3ffb7846003820f351f6813b70c300
tansibMuttakin <tansib.muhaimin@gmail.com>
2025-04-12 18:06:11 +0600

    add columns on folders table

--- database/migrations/2025_04_12_061731_create_folders_table.php
+++ database/migrations/2025_04_12_061731_create_folders_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,10 @@ return new class extends Migration
     {
         Schema::create('folders', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->foreignId('order_id')->constrained()->onDelete('cascade');
+            $table->unsignedBigInteger('parent_id')->nullable();
+            $table->foreign('parent_id')->references('id')->on('folders')->onDelete('cascade');
             $table->timestamps();
         });
     }

44a123025beeb398fe6fa622f46a7f043b511abb
tansibMuttakin <tansib.muhaimin@gmail.com>
2025-04-12 13:40:59 +0600

    create columns in Order table

--- database/migrations/2025_04_12_061720_create_orders_table.php
+++ database/migrations/2025_04_12_061720_create_orders_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,9 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->string('order_number')->unique();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('status'); #pending, in_progress, completed
             $table->timestamps();
         });
     }

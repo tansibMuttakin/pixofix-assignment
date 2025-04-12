6f74000317334909403b620ab72d69421ba27913
tansibMuttakin <tansib.muhaimin@gmail.com>
2025-04-12 18:30:15 +0600

    define relationship mehtod on Folder model

--- app/Models/Folder.php
+++ app/Models/Folder.php
@@ -20,4 +20,40 @@ class Folder extends Model
     {
         return $this->belongsTo(Order::class);
     }
+
+    /**
+     * Defines BelongsTo relation between Folder and Folder
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo<Folder, Folder>
+     */
+    public function parent()
+    {
+        return $this->belongsTo(self::class, 'parent_id');
+    }
+
+    /**
+     * Defines HasMany relation between Folder and Folder
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany<Folder, Folder>
+     */
+    public function children()
+    {
+        return $this->hasMany(self::class, 'parent_id');
+    }
+
+    /**
+     * This will give model's Parent, Parent's parent, and so on until root.  
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo<Folder, Folder>
+     */
+    public function parentRecursive()
+    {
+        return $this->parent()->with('parentRecursive');
+    }
+
+    /**
+     * This will give model's Children, Children's Children and so on until last node.
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany<Folder, Folder>
+     */
+    public function childrenRecursive()
+    {
+        return $this->children()->with('childrenRecursive');
+    }
 }

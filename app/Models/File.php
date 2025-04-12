6014b8636bf4ae3e4450878eb76953d96b96c166
tansibMuttakin <tansib.muhaimin@gmail.com>
2025-04-12 18:21:02 +0600

    create columns for files table

--- app/Models/File.php
+++ app/Models/File.php
@@ -6,5 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class File extends Model
 {
-    //
+    protected $fillable = [
+        'folder_id',
+        'order_id',
+        'file_name',
+        'file_path',
+        'status',
+    ];
+
+    /**
+     * Defines BelongsTo relation between File and User
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo<User, File>
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Defines BelongsTo relation between File and Order
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo<Order, File>
+     */
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
 }
--- database/migrations/2025_04_12_061737_create_files_table.php
+++ database/migrations/2025_04_12_061737_create_files_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,15 @@ return new class extends Migration
     {
         Schema::create('files', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('folder_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('oreder_id')->constrained()->cascadeOnDelete();
+            $table->string('file_name');
+            $table->string('file_path');
+            $table->string('status');
+            $table->unsignedBigInteger('claimed_by')->nullable();
+            $table->foreign('claimed_by')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamp('claimed_at')->nullable();
+            $table->timestamp('completed_at')->nullable();
             $table->timestamps();
         });
     }

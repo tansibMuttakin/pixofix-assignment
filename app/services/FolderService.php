7f54a72d5e39adc245b3bbd0cac16edc7cef3456
tansibMuttakin <tansib.muhaimin@gmail.com>
2025-04-13 15:00:01 +0600

    create service class and functions for Folder and File

---
+++ app/services/FolderService.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Models\Folder;
+
+class FolderService
+{
+    public static function create($folder)
+    {
+        //code
+    }
+
+    public static function update($data, Folder $folder)
+    {
+        //code
+    }
+
+    public static function delete(Folder $folder)
+    {
+        //code
+    }
+}

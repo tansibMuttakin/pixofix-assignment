44d53a4f62f14dba75421cb8c4cb8c9190f3af05
tansibMuttakin <tansib.muhaimin@gmail.com>
2025-04-13 14:54:26 +0600

    create service class and functions for order

---
+++ app/services/OrderService.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Models\Order;
+
+class OrderService
+{
+    public static function create($data)
+    {
+        //code
+    }
+
+    public static function update($data, Order $order)
+    {
+        //code
+    }
+
+    public static function delete(Order $order)
+    {
+        //code
+    }
+}

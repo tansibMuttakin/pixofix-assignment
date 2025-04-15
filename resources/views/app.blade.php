236072ca0302a0b55bb72fd05b6af17f3e29e205
tansibMuttakin <tansib.muhaimin@gmail.com>
2025-04-15 12:37:10 +0600

    use blade comment instead of html comment

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,11 +4,11 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
-    <!-- @routes
+    {{-- @routes
     @inertiaHead
     @viteReactRefresh
-    @vite(['resources/css/app.css', 'resources/js/app.js']) -->
-    @vite('resources/js/app.js')
+    @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+    @vite('resources/js/app.jsx')
     @inertiaHead
 </head>
 

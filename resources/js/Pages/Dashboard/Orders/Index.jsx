68b0bade72031cfaf41169532b19ad93a54c87a5
tansibMuttakin <tansib.muhaimin@gmail.com>
2025-04-18 10:49:22 +0600

    add create order button

--- resources/js/Pages/Dashboard/Orders/Index.jsx
+++ resources/js/Pages/Dashboard/Orders/Index.jsx
@@ -2,10 +2,14 @@ import { Head } from "@inertiajs/react";
 import React from "react";
 import Table from "@/components/Orders/Table";
 import Dashboard from "@/layouts/Dashboard";
+import { Button } from "@material-tailwind/react";
 export default function Index() {
     return (
         <>
             <Dashboard>
+                <div className="mt-12 flex justify-end">
+                    <Button variant="gradient">Create Order</Button>
+                </div>
                 <Table></Table>
             </Dashboard>
         </>

269b8baaf08e2a09f33e8eb08a499c7da77a509a
tansibMuttakin <tansib.muhaimin@gmail.com>
2025-04-18 15:20:10 +0600

    remove duplicate migration files

--- database/migrations/2025_04_12_061720_create_orders_table.php
+++ database/migrations/2025_04_12_061720_create_orders_table.php
@@ -13,8 +13,9 @@ return new class extends Migration {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->string('order_number')->unique();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->string('status'); #pending, in_progress, completed
+            $table->unsignedBigInteger('created_by')->nullable();
+            $table->foreign('created_by')->references('id')->on('users')->onDelete('cascade');
+            $table->string('status')->default('pending'); #pending, in_progress, completed
             $table->timestamps();
         });
     }

8505e6decb19a94ab2fb2ab62b1d8fb965fc8559
tansibMuttakin <tansib.muhaimin@gmail.com>
2025-04-18 15:00:13 +0600

    add modal for create order

--- resources/js/Pages/Dashboard/Orders/Index.jsx
+++ resources/js/Pages/Dashboard/Orders/Index.jsx
@@ -1,14 +1,21 @@
-import { Head } from "@inertiajs/react";
 import React from "react";
 import Table from "@/components/Orders/Table";
 import Dashboard from "@/layouts/Dashboard";
 import { Button } from "@material-tailwind/react";
+import { CreateOrderModal } from "../../../components/Orders/CreateOrderModal";
 export default function Index() {
+    const [open, setOpen] = React.useState(false);
+
+    const handleOpen = () => setOpen(!open);
+
     return (
         <>
             <Dashboard>
-                <div className="mt-12 flex justify-end">
-                    <Button variant="gradient">Create Order</Button>
+                <div className="mt-12 mb-12 flex justify-end">
+                    <Button variant="gradient" onClick={handleOpen}>
+                        Create Order
+                    </Button>
+                    <CreateOrderModal open={open} handleOpen={handleOpen} />
                 </div>
                 <Table></Table>
             </Dashboard>
